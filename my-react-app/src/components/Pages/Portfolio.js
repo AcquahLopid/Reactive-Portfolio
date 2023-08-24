df51c0c2dbb927038695859669604bc1ad787d18
Emanuel Molina <128034917+AcquahLopid@users.noreply.github.com>
2023-08-24 13:27:38 -0500

    add c2c project and the password generator project to my portfolio

--- my-react-app/src/components/Pages/Portfolio.js
+++ my-react-app/src/components/Pages/Portfolio.js
@@ -6,6 +6,8 @@ import GoToTop from '../Utils/GoToTop';
 
 // project images
 import Emojify from "../Assets/Emojify project.png";
+import C2C from "../Assets/C2C Screenshot.png";
+import PasswordGen from "../Assets/Password generator.png";
 
 
 const Portfolio = () => {
@@ -20,10 +22,10 @@ const Portfolio = () => {
     <div className="row row-cols-1 row-cols-md-3 g-4">
         <div className="col">
             <div className="card bg-success h-100 overflow-hidden d-flex flex-column">
-                <img src={Emojify} className="card-img-top" alt="Emojify Project" />
+                <img src={Emojify} className="rounded-bottom-4 card-img-top" alt="Emojify Project" />
                 <div className="card-body d-flex flex-column">
-                    <h5 className="card-title fs-4 ">Emojify</h5>
-                    <p className="card-text fs-6 ">
+                    <h5 className="card-title fs-2 ">Emojify</h5>
+                    <p className="card-text fs-4 ">
                     This app helps users choose movies more easily based on their emotions expressed through emojis.
                      Emojis are connected to genres (e.g., Skull for horror). Users pick emojis, get movie suggestions,
                       and can also explore past recommendations.
@@ -32,7 +34,7 @@ const Portfolio = () => {
                         <button className="btn btn-light" type="button" onClick={() => (window.location.href = "https://samhiga.github.io/emojify/")}>
                             Deployed Site
                         </button>
-                        <button className="btn btn-light" type="button" onClick={() => (window.location.href = "https://github.com/samhiga/emojify")}>
+                        <button className="btn btn-light" type="button" onClick={() => (window.location.href = "https://github.com/AcquahLopid/emojify")}>
                             Repository
                         </button>
                     </div>
@@ -41,15 +43,19 @@ const Portfolio = () => {
         </div>
         <div className="col">
             <div className="card bg-success h-100 overflow-hidden d-flex flex-column">
-                <img src={Emojify} className="card-img-top" alt="Another Project" />
+                <img src={C2C} className="rounded-bottom-4 card-img-top" alt="C2C Project" />
                 <div className="card-body d-flex flex-column">
-                    <h5 className="card-title fs-4 text-truncate">Another Project</h5>
-                    <p className="card-text fs-6 text-truncate"> hello</p>
+                    <h5 className="card-title fs-2 text-truncate">C2C</h5>
+                    <p className="card-text fs-4"> 
+                    Small business website that allows clients to message with business owners. Clients can register or login in
+                    with their already created accounts. This website is a good framework for future websites people want to build. <br/>
+                    (Admin Login: admin@email.com | Password: password) <br/> (Client Login: email1@email.com | Password: password1)
+                    </p>
                     <div className="d-grid gap-2 mt-auto">
-                        <button className="btn btn-light" type="button" onClick={() => (window.location.href = "https://another-project-deployed-site/")}>
+                        <button className="btn btn-light" type="button" onClick={() => (window.location.href = "https://small-business-project-client.herokuapp.com/")}>
                             Deployed Site
                         </button>
-                        <button className="btn btn-light" type="button" onClick={() => (window.location.href = "https://github.com/username/another-project")}>
+                        <button className="btn btn-light" type="button" onClick={() => (window.location.href = "https://github.com/AcquahLopid/small-business-project")}>
                             Repository
                         </button>
                     </div>
@@ -58,7 +64,7 @@ const Portfolio = () => {
         </div>
         <div className="col">
             <div className="card bg-success h-100 overflow-hidden d-flex flex-column">
-                <img src={Emojify} className="card-img-top" alt="Another Project" />
+                <img src={Emojify} className="rounded-bottom-4 card-img-top" alt="Another Project" />
                 <div className="card-body d-flex flex-column">
                     <h5 className="card-title fs-4 text-truncate">Another Project</h5>
                     <p className="card-text fs-6 text-truncate"> hello</p>
@@ -82,19 +88,19 @@ const Portfolio = () => {
     <div className="row row-cols-1 row-cols-md-3 g-4">
         <div className="col">
             <div className="card bg-success h-100 overflow-hidden d-flex flex-column">
-                <img src={Emojify} className="card-img-top" alt="Emojify Project" />
+                <img src={PasswordGen} className="rounded-bottom-4 card-img-top" alt="Password Generator Project" />
                 <div className="card-body d-flex flex-column">
-                    <h5 className="card-title fs-4 ">Emojify</h5>
+                    <h5 className="card-title fs-4 ">Password Generator</h5>
                     <p className="card-text fs-6 ">
-                    This app helps users choose movies more easily based on their emotions expressed through emojis.
-                     Emojis are connected to genres (e.g., Skull for horror). Users pick emojis, get movie suggestions,
-                      and can also explore past recommendations.
+                    This website helps user make a Passwords. first it ask users to appect conditions of their password.
+                    ex. if they want spaces in it, or how long should it be. then the program generates users a password randomly
+                    based on their conditions.
                       </p>
                     <div className="d-grid gap-2 mt-auto">
-                        <button className="btn btn-light" type="button" onClick={() => (window.location.href = "https://samhiga.github.io/emojify/")}>
+                        <button className="btn btn-light" type="button" onClick={() => (window.location.href = "https://acquahlopid.github.io/password-generator/")}>
                             Deployed Site
                         </button>
-                        <button className="btn btn-light" type="button" onClick={() => (window.location.href = "https://github.com/samhiga/emojify")}>
+                        <button className="btn btn-light" type="button" onClick={() => (window.location.href = "https://github.com/AcquahLopid/password-generator")}>
                             Repository
                         </button>
                     </div>
@@ -103,7 +109,7 @@ const Portfolio = () => {
         </div>
         <div className="col">
             <div className="card bg-success h-100 overflow-hidden d-flex flex-column">
-                <img src={Emojify} className="card-img-top" alt="Another Project" />
+                <img src={Emojify} className="rounded-bottom-4 card-img-top" alt="Another Project" />
                 <div className="card-body d-flex flex-column">
                     <h5 className="card-title fs-4 text-truncate">Another Project</h5>
                     <p className="card-text fs-6 text-truncate"> hello</p>
@@ -120,7 +126,7 @@ const Portfolio = () => {
         </div>
         <div className="col">
             <div className="card bg-success h-100 overflow-hidden d-flex flex-column">
-                <img src={Emojify} className="card-img-top" alt="Another Project" />
+                <img src={Emojify} className="rounded-bottom-4 card-img-top" alt="Another Project" />
                 <div className="card-body d-flex flex-column">
                     <h5 className="card-title fs-4 text-truncate">Another Project</h5>
                     <p className="card-text fs-6 text-truncate"> hello</p>
